ecc4c216e08198595de8a4368fcb05837b8710dd
Diffusicle <dudrjs7171@gmail.com>
2020-05-13 12:31:36 +0900

    Back Button

--- app/src/main/java/com/example/aone/Presenter/EditActivity.java
+++ app/src/main/java/com/example/aone/Presenter/EditActivity.java
@@ -1,8 +1,10 @@
 package com.example.aone.Presenter;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.aone.R;
 
@@ -12,5 +14,22 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.main_toolbar_edit);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
