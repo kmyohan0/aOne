b29c71d007ac83a2c82185b021056077faa87863
Diffusicle <dudrjs7171@gmail.com>
2020-05-13 12:01:11 +0900

    EditActivity Start

---
+++ app/src/main/java/com/example/aone/Presenter/EditActivity.java
@@ -0,0 +1,16 @@
+package com.example.aone.Presenter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aone.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
