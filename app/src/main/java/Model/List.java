ba7402623708caef764c92fdb79e024a888cd0fa
Yohan Kim <yohan.kim@email.ucr.edu>
2020-05-12 19:49:51 -0700

    Created List Class

--- app/src/main/java/Model/List.java
+++ app/src/main/java/Model/List.java
@@ -1,4 +1,50 @@
 package Model;
+import java.util.Calendar;
+import java.util.Date;
 
 public class List {
+
+    private Date currentDate, dueDate;
+    //private Category  category;
+    private String ActivityName;
+
+    public List() {
+        currentDate = null;
+        dueDate = null;
+        ActivityName = null;
+    }
+
+    public List(Date CurrentDate, String ActivityName) {
+        this.ActivityName = ActivityName;
+    }
+
+    public List(Date CurrentDate, Date dueDate, String ActivityName) {
+        this.currentDate = CurrentDate;
+        this.dueDate = dueDate;
+        this.ActivityName = ActivityName;
+    }
+
+    public Date getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(Date currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getActivityName() {
+        return ActivityName;
+    }
+
+    public void setActivityName(String activityName) {
+        ActivityName = activityName;
+    }
 }
